db309761b8b67eef41e1d872acd51808b9d17367
farrellkai <farrellkai@gmail.com>
2024-04-15 11:20:37 -0400

    number cache

--- algos.js
+++ algos.js
@@ -1,1 +1,15 @@
-const uncompres = (s) => {};
+const uncompress = (s) => {
+  //create number cache
+  const numCache = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+  };
+};
