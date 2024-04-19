f9a87c123f172edbca5a765520e0822e91c6fc50
farrellkai <farrellkai@gmail.com>
2024-04-19 09:46:31 -0400

    pairProduct function

--- algos.js
+++ algos.js
@@ -218,3 +218,6 @@ console.log(pairSum([4, 7, 9, 2, 5, 1], 3));
 console.log(pairSum([1, 6, 7, 2], 13));
 console.log(pairSum([9, 9], 18));
 console.log(pairSum([6, 4, 2, 8], 12));
+
+//pairProduct function
+const pairProduct = (nums, targetProd) => {};
