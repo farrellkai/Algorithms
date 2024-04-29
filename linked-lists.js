a63bd64964db2744f78e6f5d5b9d3b313b510d98
farrellkai <farrellkai@gmail.com>
2024-04-29 11:52:28 -0400

    alternate getNodeVal recursive case

--- linked-lists.js
+++ linked-lists.js
@@ -244,4 +244,6 @@ console.log(getNodeVal(node1, 1));
 const getNodeVal2 = (head, index) => {
   //base case: if index is 0 return head value
   if (index === 0) return head.val;
+  //return function with next node and index minus 1 as arguments
+  return getNodeVal2(head.next, --index);
 };
