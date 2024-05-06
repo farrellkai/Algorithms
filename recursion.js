532c5dcb35072e6a53c53cdb3d5d28500acd56b1
farrellkai <farrellkai@gmail.com>
2024-05-06 15:09:14 -0400

    recursive case

--- recursion.js
+++ recursion.js
@@ -2,4 +2,6 @@
 const sumNumbers = (numbers) => {
   //base case: if array is empty return 0
   if (!numbers.length) return 0;
+  //recursive case: reutrn sum of last element plus invoked function with mutated array as argument
+  return numbers.pop() + sumNumbers(numbers);
 };
