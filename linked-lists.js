30221516ae54b67b19f7c0eb908040f6fac84d77
farrellkai <farrellkai@gmail.com>
2024-05-13 14:29:57 -0400

    univalue test cases

--- linked-lists.js
+++ linked-lists.js
@@ -393,3 +393,59 @@ const univalue = (head) => {
   //if algo made it this far return true
   return true;
 };
+
+//test cases
+// const a = new Node(7);
+// const b = new Node(7);
+// const c = new Node(7);
+
+// a.next = b;
+// b.next = c;
+// console.log(univalue(a));
+
+// const a = new Node(7);
+// const b = new Node(7);
+// const c = new Node(4);
+
+// a.next = b;
+// b.next = c;
+// console.log(univalue(a));
+
+// const u = new Node(2);
+// const v = new Node(2);
+// const w = new Node(2);
+// const x = new Node(2);
+// const y = new Node(2);
+
+// u.next = v;
+// v.next = w;
+// w.next = x;
+// x.next = y;
+// console.log(univalue(u));
+
+// const u = new Node(2);
+// const v = new Node(2);
+// const w = new Node(3);
+// const x = new Node(3);
+// const y = new Node(2);
+
+// u.next = v;
+// v.next = w;
+// w.next = x;
+// x.next = y;
+// console.log(univalue(u));
+
+// const z = new Node('z');
+// console.log(univalue(z));
+
+// const u = new Node(2);
+// const v = new Node(1);
+// const w = new Node(2);
+// const x = new Node(2);
+// const y = new Node(2);
+
+// u.next = v;
+// v.next = w;
+// w.next = x;
+// x.next = y;
+// console.log(univalue(u));
