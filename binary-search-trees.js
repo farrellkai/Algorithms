0dcc0402b1ae8a63a9ddc7031e3c3f40543c26ca
farrellkai <farrellkai@gmail.com>
2024-05-21 10:21:10 -0400

    depthFirstValues recursive case

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -11,4 +11,8 @@ class Node {
 const depthFirstValues = (root) => {
   //base case: if root is null return empty array
   if (!root) return [];
+  //base case: if root has no children return array containing root value
+  if (!root.left && root.right) return [root.val];
+  //recursive case: return concatination of result of function on left node and right node
+  return depthFirstValues(root.left).concat(depthFirstValues(root.right));
 };
