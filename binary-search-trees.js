848994ae2562817973c53de1304cba324a6aba45
farrellkai <farrellkai@gmail.com>
2024-05-23 09:58:41 -0400

    treeIncludes test cases

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -248,3 +248,28 @@ const treeIncludes = (root, target) => {
     ? true
     : false;
 };
+
+//test cases
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(treeIncludes(a, 'e'));
+console.log(treeIncludes(a, 'a'));
+console.log(treeIncludes(a, 'n'));
+const g = new Node('g');
+const h = new Node('h');
+
+e.left = g;
+f.right = h;
+console.log(treeIncludes(a, 'f'));
+console.log(treeIncludes(a, 'p'));
+console.log(treeIncludes(null, 'b'));
