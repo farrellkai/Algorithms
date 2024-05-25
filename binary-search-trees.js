13c02832949d9d792db3cf148d0c581f580ec74e
farrellkai <farrellkai@gmail.com>
2024-05-25 09:41:15 -0400

    treeValueCount test cases

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -448,20 +448,48 @@ const treeValueCount = (root, target) => {
 };
 
 //test cases
-const a = new Node(12);
-const b = new Node(6);
-const c = new Node(6);
-const d = new Node(4);
-const e = new Node(6);
-const f = new Node(12);
+// const a = new Node(12);
+// const b = new Node(6);
+// const c = new Node(6);
+// const d = new Node(4);
+// const e = new Node(6);
+// const f = new Node(12);
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
-console.log(treeValueCount(a, 6));
-console.log(treeValueCount());
-console.log(treeValueCount());
-console.log(treeValueCount());
-console.log(treeValueCount());
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// console.log(treeValueCount(a, 6));
+// const a = new Node(12);
+// const b = new Node(6);
+// const c = new Node(6);
+// const d = new Node(4);
+// const e = new Node(6);
+// const f = new Node(12);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// console.log(treeValueCount(a, 12));
+// const a = new Node(7);
+// const b = new Node(5);
+// const c = new Node(1);
+// const d = new Node(1);
+// const e = new Node(8);
+// const f = new Node(7);
+// const g = new Node(1);
+// const h = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+// console.log(treeValueCount(a, 1));
+//console.log(treeValueCount(a, 9));
+//console.log(treeValueCount(null, 42));
