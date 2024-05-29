4eb2dace2e484357fc02c2b5c941bafe8e35a5ea
farrellkai <farrellkai@gmail.com>
2024-05-29 10:57:14 -0400

    botRightValue test cases

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -546,3 +546,69 @@ const botRightValue = (root) => {
   }
   return cur;
 };
+
+//test cases
+// const a = new Node(3);
+// const b = new Node(11);
+// const c = new Node(10);
+// const d = new Node(4);
+// const e = new Node(-2);
+// const f = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// console.log(botRightValue(a));
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(6);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// e.right = h;
+// console.log(botRightValue(a));
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(6);
+// const i = new Node(7);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// e.right = h;
+// f.left = i;
+// console.log(botRightValue(a));
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+
+// a.left = b;
+// a.right = c;
+// b.right = d;
+// d.left = e;
+// e.right = f;
+// console.log(botRightValue(a));
+// const a = new Node(42);
+// console.log(botRightValue(a));
