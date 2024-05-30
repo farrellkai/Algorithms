3194a32fd3a11f5320d584632b746de353d7c6c2
farrellkai <farrellkai@gmail.com>
2024-05-30 10:34:30 -0400

    allTreePaths test cases

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -636,3 +636,41 @@ const allTreePaths = (root) => {
   }
   return bothSides;
 };
+
+//test cases
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+console.log(allTreePaths(a));
+const g = new Node('g');
+const h = new Node('h');
+const i = new Node('i');
+
+e.left = g;
+e.right = h;
+f.left = i;
+console.log(allTreePaths(a));
+const q = new Node('q');
+const r = new Node('r');
+const s = new Node('s');
+const t = new Node('t');
+const u = new Node('u');
+const v = new Node('v');
+
+q.left = r;
+q.right = s;
+r.right = t;
+t.left = u;
+u.right = v;
+console.log(allTreePaths(q));
+const z = new Node('z');
+console.log(allTreePaths(z));
