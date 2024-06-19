d53ef4c442948efe462a54b1a3e5486b69f210a1
farrellkai <farrellkai@gmail.com>
2024-06-19 16:13:16 -0400

    hasPathDepth base case

--- graphs.js
+++ graphs.js
@@ -1,2 +1,5 @@
 //has path function depth first
-const hasPathDepth = (graph, start, fin) => {};
+const hasPathDepth = (graph, start, fin) => {
+  //base case: if start matches fin return true
+  if (start === fin) return true;
+};
