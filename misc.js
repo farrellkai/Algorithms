03f42493f62e5df863165e0c9193b5d126bdeb28
farrellkai <farrellkai@gmail.com>
2026-09-16 09:45:23 -0400

    second ternary

--- misc.js
+++ misc.js
@@ -101,6 +101,9 @@ const anagrams = (str1, str2) => {
     map1.has(str1[i])
       ? map1.set(str1[i], map1.get(str1[i]) + 1)
       : map1.set(str1[i], 1);
+    map2.has(str2[i])
+      ? map2.set(str2[i], map2.get(str2[i]) + 1)
+      : map2.set(str2[i], 1);
     i++;
   }
 };
