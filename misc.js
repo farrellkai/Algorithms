729dff7205210e74717888aa797dd9df340969a0
farrellkai <farrellkai@gmail.com>
2026-09-17 16:09:28 -0400

    change right pointer

--- misc.js
+++ misc.js
@@ -316,5 +316,6 @@ const binarySearch = (arr, target) => {
     if (arr[left] === target) return left;
     if (arr[right] === target) return right;
     if (arr[mid] === target) return mid;
+    if (target < arr[mid]) right = mid;
   }
 };
