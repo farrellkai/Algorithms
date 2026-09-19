a14fb7ec72a9c2448be17d080cdf851bfc102f5c
farrellkai <farrellkai@gmail.com>
2026-09-19 08:22:33 -0400

    longest variable

--- misc.js
+++ misc.js
@@ -392,4 +392,6 @@ const largestWindowSum = (arr, k) => {
 
 // Return the length of the longest substring containing no repeated characters.
 
-const longestSubString = (str) => {};
+const longestSubString = (str) => {
+  let longest = 0;
+};
