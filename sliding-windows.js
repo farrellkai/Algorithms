da3ce99217635c4fedd41a5d3ea2df544b57982f
farrellkai <farrellkai@gmail.com>
2026-09-23 10:54:33 -0400

    return output

--- sliding-windows.js
+++ sliding-windows.js
@@ -151,7 +151,7 @@ const findSubarraySum = (arr, target) => {
       left++;
       output[0] = left;
     }
-    if (sum === target) return [left, right];
+    if (sum === target) return output;
   }
   return [];
 }
