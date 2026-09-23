457aa8fa757c3cd74ef7c9671c1ceb1d3942c44f
farrellkai <farrellkai@gmail.com>
2026-09-23 10:23:18 -0400

    while loop

--- sliding-windows.js
+++ sliding-windows.js
@@ -109,4 +109,9 @@ const countSubstringAnagrams = (str, anagram) => {
 
   const anagramArr = new Array(26).fill(0);
   const stringArr = new Array(26).fill(0);
+
+  let i = 0;
+  while (i < anagram.length) {
+    i++;
+  }
 };
