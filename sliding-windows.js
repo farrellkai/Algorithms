fb199db5c7a924f4ffab015366365ce86ebcf0cb
farrellkai <farrellkai@gmail.com>
2026-09-24 08:43:18 -0400

    conditional

--- sliding-windows.js
+++ sliding-windows.js
@@ -203,5 +203,8 @@ const longestUniqueSubstring = (str) => {
 
   let left = 0;
   let right = 0;
-  while (right < str.length) {}
+  while (right < str.length) {
+    if (!set.has(str[right])) {
+    }
+  }
 };
